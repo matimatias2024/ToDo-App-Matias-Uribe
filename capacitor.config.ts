4180be39c306571a585f5f3b45a69068b8289b74
matiasuribe2017 <matimatias2024@gmail.com>
2025-10-18 16:32:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aiep.todoapp',
+  appName: 'todo-aiep',
+  webDir: 'www'
+};
+
+export default config;
